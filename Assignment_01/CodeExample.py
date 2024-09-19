88b1d01b578be9f037e7cb07ac67f82762fe231d
mitchelldmartin <164359892+mitchelldmartin@users.noreply.github.com>
2024-09-18 20:52:23 -0400

    Add files via upload

---
+++ Assignment_01/CodeExample.py
@@ -0,0 +1,21 @@
+# Martin, Mitchell
+# This is a screenshot of the 79 char ruler.
+
+# Max Characters is 79
+max_char = 79
+
+# This will print out the number 79
+print(max_char)
+
+# This will print out 0 (as a string) x 79 times.
+print('0' * max_char)
+
+# your printout will not match the line because the Terminal font is not the
+# same size as the VS code font. But if you copy it and paste it will fit!
+
+'''
+00000000000000000000000000000000000000000000000000000000000000000000000000000
+'''
+
+# This will print out the integer zero.
+print(0 * 79)
