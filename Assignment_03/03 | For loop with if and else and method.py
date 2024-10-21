dae3a4ac7b7e72c5161b579812d96f035709b9cf
mitchelldmartin <164359892+mitchelldmartin@users.noreply.github.com>
2024-10-21 13:07:30 -0400

    Add files via upload

---
+++ Assignment_03/03 | For loop with if and else and method.py
@@ -0,0 +1,8 @@
+players = ['lebron', 'curry', 'jordan', 'kobe']
+
+for player in players:
+    if player == 'jordan':
+        print(player.title())  # Applies the title case to 'jordan'
+    else:
+        print(player.upper())  # Applies the upper case to all other players
+
